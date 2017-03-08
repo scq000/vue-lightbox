ad52f7406d0468c48cfeb9b3b7a081f4c9e1a297
scq000 <scq000@hotmail.com>
2017-03-08 15:17:55 +0800

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<lightbox></lightbox>
+	</div>
+</template>
+
+<script>
+import Lightbox from './components/Lightbox';
+
+export default {
+    name: 'app',
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        Lightbox,
+    },
+};
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ src/components/Lightbox.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="gallery">
+      <div class="thumbnail" v-for="n in 4">
+          <a :href="'static/' + n + '.jpg'"><img :src="'static/' + n + '.jpg'"></a>
+      </div>
+    </div>
+</template>
+
+<script>
+const $ = require('jquery');
+require('../../lib/jquery.lightbox.js');
+require('../../lib/jquery.lightbox.css');
+
+export default {
+    name: 'lightbox',
+    mounted() {
+        $(this.$el).find('a').lightbox();
+    },
+};
+</script>
+
+<style lang="less" scoped>
+
+.gallery {
+    display: flex;
+    align-items: center;
+    height: 200px;
+    overflow: hidden;
+
+    a {
+        text-decoration: none;
+
+        img {
+            display: block;
+            max-width: 100%;
+            height: auto;
+            vertical-align: middle;
+
+            &:hover, &:focus {
+                cursor: zoom-in;
+            }
+        }
+    }
+
+}
+
+
+</style>
